b451ef6e6156084229664d093b4149c22f959f88
Rahil Patel <rahilpatel99@gmail.com>
2018-10-20 07:09:51 -0400

    login page UI

--- DangerPop/Colors.swift
+++ DangerPop/Colors.swift
@@ -10,6 +10,12 @@ import Foundation
 import UIKit
 
 struct Colors {
-    static let orange = UIColor.init(red: 255, green: 165, blue: 0, alpha: 1)
-    static let black = UIColor.init(red: 255, green: 255, blue: 255, alpha: 1)
+    static let orange = UIColor.init(r: 255, g: 157, b: 45, a: 1)
+    static let black = UIColor.init(r: 0, g: 0, b: 0, a: 1)
+}
+
+extension UIColor {
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: a)
+    }
 }
--- DangerPop/ViewController.swift
+++ DangerPop/ViewController.swift
@@ -9,12 +9,118 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+extension UIButton {
+    func leftImage(image: UIImage, renderMode: UIImage.RenderingMode) {
+        self.setImage(image.withRenderingMode(renderMode), for: .normal)
+        self.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: image.size.width / 2)
+        self.contentHorizontalAlignment = .left
+        self.imageView?.contentMode = .scaleAspectFit
+    }
+    
+    func rightImage(image: UIImage, renderMode: UIImage.RenderingMode){
+        self.setImage(image.withRenderingMode(renderMode), for: .normal)
+        self.imageEdgeInsets = UIEdgeInsets(top: 0, left:image.size.width / 2, bottom: 0, right: 0)
+        self.contentHorizontalAlignment = .right
+        self.imageView?.contentMode = .scaleAspectFit
+    }
+}
 
+class ViewController: UIViewController, UITextFieldDelegate {
+    let emailFld = UITextField()
+    let passFld = UITextField()
+    let loginBtn = UIButton()
+    let separator = UIView()
+    let googleBtn = UIButton()
+    let createAccountBtn = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    
+    func setupUI() {
+        let viewSize = view.frame.size
+        let underline = CALayer()
+        let underline2 = CALayer()
+        let width = CGFloat(2.0)
+        let edgeInset:CGFloat = 1.0/12
 
+        view.backgroundColor = Colors.black
+        
+        
+        emailFld.frame = CGRect(origin: CGPoint(x: viewSize.width * edgeInset, y: viewSize.height / 4), size: CGSize(width: (viewSize.width - (2 * edgeInset) * viewSize.width), height: 25))
+        emailFld.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        emailFld.textColor = UIColor.white
+        emailFld.textAlignment = .center
+        emailFld.tag = 0;
+        
+        passFld.frame = CGRect(origin: CGPoint(x: emailFld.frame.minX, y: emailFld.frame.maxY + 40), size: CGSize(width: emailFld.frame.size.width, height: 25))
+        passFld.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        passFld.isSecureTextEntry = true
+        passFld.textColor = UIColor.white
+        passFld.textAlignment = .center
+        passFld.tag = 1
+        
+        // textfield underline
+        underline.borderColor = Colors.orange.cgColor
+        underline.frame = CGRect(x: 0, y: emailFld.frame.size.height - width, width: emailFld.frame.size.width, height: emailFld.frame.size.height)
+        underline.borderWidth = width
+        emailFld.layer.addSublayer(underline)
+        emailFld.layer.masksToBounds = true
+        
+        underline2.borderColor = Colors.orange.cgColor
+        underline2.frame = CGRect(x: 0, y: passFld.frame.size.height - width, width: passFld.frame.size.width, height: passFld.frame.size.height)
+        underline2.borderWidth = width
+        passFld.layer.addSublayer(underline2)
+        passFld.layer.masksToBounds = true
+        
+        loginBtn.frame = CGRect(origin: CGPoint(x: passFld.frame.minX, y: passFld.frame.maxY + 40), size: CGSize(width: passFld.frame.size.width, height: 40))
+        loginBtn.setTitle("Login", for: .normal)
+        loginBtn.setTitleColor(.white, for: .normal)
+        loginBtn.backgroundColor = Colors.orange
+        loginBtn.layer.cornerRadius = 4
+        
+        //separator.borderColor = Colors.orange.cgColor
+        separator.backgroundColor = Colors.orange
+        separator.frame = CGRect(x: viewSize.width * edgeInset * 0.5, y: loginBtn.frame.maxY + 20, width: (viewSize.width - (edgeInset) * viewSize.width), height: 1)
+        
+        let btnWidth = CGFloat(170)
+        googleBtn.frame = CGRect(x: (viewSize.width - btnWidth) / 2, y: separator.frame.maxY + 20, width: btnWidth, height: 40)
+        googleBtn.setImage(UIImage(named: "SignInWithGoogle"), for: .normal)
+        if let imgView = googleBtn.imageView {
+            imgView.layer.cornerRadius = 4
+        }
+        googleBtn.subviews.first?.contentMode = .scaleAspectFit
+        googleBtn.backgroundColor = UIColor.white
+        googleBtn.layer.cornerRadius = 4
+        
+        createAccountBtn.frame = CGRect(x: (viewSize.width - btnWidth) / 2, y: googleBtn.frame.maxY + 20, width: btnWidth, height: 40)
+        createAccountBtn.backgroundColor = Colors.orange
+        createAccountBtn.setTitle("Create an account", for: .normal)
+        createAccountBtn.setTitleColor(.white, for: .normal)
+        createAccountBtn.layer.cornerRadius = 4
+        
+        view.addSubview(emailFld)
+        view.addSubview(passFld)
+        view.addSubview(loginBtn)
+        view.addSubview(separator)
+        view.addSubview(googleBtn)
+        view.addSubview(createAccountBtn)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.placeholder = nil
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.tag == 0 && textField.text == nil {
+            textField.placeholder = "Email"
+        }
+        if textField.tag == 1 && textField.text == nil {
+            textField.placeholder = "Password"
+        }
+    }
 }
 
