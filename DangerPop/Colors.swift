3bd7a90553d68fc0cd62bdc8a4014f708bc6115e
Rahil Patel <rahilpatel99@gmail.com>
2018-10-20 03:19:35 -0400

    added Colors class

---
+++ DangerPop/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  DangerPop
+//
+//  Created by Rahil Patel on 10/20/18.
+//  Copyright © 2018 DangerPros. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    static let orange = UIColor.init(red: 255, green: 165, blue: 0, alpha: 1)
+    static let black = UIColor.init(red: 255, green: 255, blue: 255, alpha: 1)
+}
